25fdb643ed46beea94d8b6ebbfe0983d1aea306a
ElordaEcoSystem <elordaecosystem_it@mail.ru>
2025-07-29 13:12:50 +0500

    docker

--- react-client/vite.config.ts
+++ react-client/vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build:{
+    outDir: "build",
+    sourcemap: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
